617bc79d8f0809d763cd5b9c2ff9f6df546049dd
Claudio Ortolina <claudio@new-bamboo.co.uk>
2015-02-03 23:12:51 +0000

    Generates project.

---
+++ src/scripts/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = React.createClass({
+  render (){
+    return (
+      <section className="main">
+        <h1>Crochet</h1>
+      </section>
+    );
+  }
+});
+
+export default App;
