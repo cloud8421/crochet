711bf991918e752d626ad5371a31f8a41ab2639d
Claudio Ortolina <claudio@new-bamboo.co.uk>
2015-02-04 18:56:02 +0000

    Adds BaseStore.

---
+++ src/scripts/stores/_base.js
@@ -0,0 +1,17 @@
+import EventEmitter from "../utils/event-emitter";
+
+const CHANGE_EVENT = 'change';
+
+class Base extends EventEmitter {
+  emitChange() {
+    this.emit(CHANGE_EVENT);
+  }
+  addChangeListener(cb) {
+    this.on(CHANGE_EVENT, cb);
+  }
+  removeChangeListener(cb) {
+    this.off(CHANGE_EVENT, cb);
+  }
+}
+
+export default Base;
