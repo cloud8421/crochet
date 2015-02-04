95b22f9bd47cebd8c4fcd234d5ad9a8e76e2c70c
Claudio Ortolina <claudio@new-bamboo.co.uk>
2015-02-04 08:55:56 +0000

    Adds PureRenderMixin to Square.

--- src/scripts/components/Square.js
+++ src/scripts/components/Square.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React from 'react/addons';
+
+const PureRenderMixin = React.addons.PureRenderMixin;
 
 const Square = React.createClass({
+  mixins: [PureRenderMixin],
   render (){
     return (
       <p>{this.props.colors.join(',')}</p>
