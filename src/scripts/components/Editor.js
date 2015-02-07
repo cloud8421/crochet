fce9b24b94d1ae3f7f7c7dc3dc7ad184a2323631
Claudio Ortolina <claudio@new-bamboo.co.uk>
2015-02-07 12:23:19 +0000

    Makes width dynamic.

--- src/scripts/components/Editor.js
+++ src/scripts/components/Editor.js
@@ -5,11 +5,17 @@ import LayoutActions from '../actions/LayoutActions';
 import ColorStore from '../stores/Color';
 
 const Editor = React.createClass({
+  getInitialState() {
+    return {
+      width: 8
+    }
+  },
   render (){
     return (
       <div>
         <button onClick={this.addNewColor}>Add a new color</button>
         <button onClick={this.generateLayout}>Generate layout</button>
+        <input type="text" value={this.state.width} onChange={this.handleWidthChange} />
         <Colors />
       </div>
     );
@@ -17,9 +23,13 @@ const Editor = React.createClass({
   addNewColor() {
     ColorActions.addColor('#ABABAB');
   },
+  handleWidthChange: function(event) {
+    this.setState({width: event.target.value});
+  },
   generateLayout() {
     let colorsCount = ColorStore.getColors().size;
-    LayoutActions.generateLayout(64, colorsCount);
+    let squaresCount = this.state.width * this.state.width;
+    LayoutActions.generateLayout(squaresCount, colorsCount);
   }
 });
 
