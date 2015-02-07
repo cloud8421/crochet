413af3981329d8365e8885f0b7bf319ec09cbd59
Claudio Ortolina <claudio@new-bamboo.co.uk>
2015-02-07 15:38:39 +0000

    Moves "add new color" near existing colors.

--- src/scripts/components/Editor.js
+++ src/scripts/components/Editor.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Colors from './editor/Colors';
-import ColorActions from '../actions/ColorActions';
 import LayoutActions from '../actions/LayoutActions';
 import ColorStore from '../stores/Color';
 
@@ -13,15 +12,12 @@ const Editor = React.createClass({
   render (){
     return (
       <div>
-        <button onClick={this.addNewColor}>Add a new color</button>
         <button onClick={this.generateLayout}>Generate layout</button>
-        <input type="text" value={this.state.width} onChange={this.handleWidthChange} />
+        <label htmlFor="width">Width</label>
+        <input type="text" id="width" value={this.state.width} onChange={this.handleWidthChange} />
         <Colors />
       </div>
     );
-  },
-  addNewColor() {
-    ColorActions.addColor('#ABABAB');
   },
   handleWidthChange: function(event) {
     this.setState({width: event.target.value});
--- src/scripts/components/editor/Colors.js
+++ src/scripts/components/editor/Colors.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ColorStore from '../../stores/Color';
+import ColorActions from '../../actions/ColorActions';
 import ColorComponent from './Color';
 
 let getState = function() {
@@ -16,13 +17,21 @@ const Colors = React.createClass({
   _onChange() {
     this.setState(getState());
   },
+  addNewColor() {
+    ColorActions.addColor('#ABABAB');
+  },
   render() {
     let colors = this.state.colors.map(function(color) {
       return <ColorComponent color={color} />
     });
 
     return (
-      <ul className='colors'>{colors}</ul>
+      <ul className='colors'>
+        {colors}
+        <li className="add-new">
+          <button onClick={this.addNewColor}>Add a new color</button>
+        </li>
+      </ul>
     )
   }
 });
