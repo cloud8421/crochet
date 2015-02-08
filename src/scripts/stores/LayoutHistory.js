5093f8d2a91cad7ce9ab058d9f49241b20ef12e2
Claudio Ortolina <claudio@new-bamboo.co.uk>
2015-02-08 21:38:35 +0000

    Layout History binds to events.

--- src/scripts/stores/LayoutHistory.js
+++ src/scripts/stores/LayoutHistory.js
@@ -1,28 +1,56 @@
 import Immutable from "immutable";
 import BaseStore from "./_base";
 import LayoutStore from "./Layout";
+import ColorStore from "./Color";
 import AppDispatcher from "../dispatcher/AppDispatcher";
 import LayoutActionTypes from "../constants/LayoutConstants";
 
 const LayoutRecord = Immutable.Record({
-  layout: null,
+  layout: Immutable.List([]),
   width: 0,
-  height: 0
+  height: 0,
+  colors: Immutable.List([])
 });
 
 let layouts = Immutable.List([]);
+
+_saveLayout = function() {
+  newLayout = new LayoutRecord({
+    layout: LayoutStore.getLayout(),
+    width: LayoutStore.getWidth(),
+    height: LayoutStore.getHeight(),
+    colors: ColorStore.getColors()
+  });
+
+  layouts = layouts.push(newLayout);
+}
 
 class _LayoutHistory extends BaseStore {
   getLayouts() {
     return layouts;
   }
-  addLayout(layoutData) {
-    let newLayout = new LayoutRecord(layoutData);
-    layouts = layouts.push(newLayout);
-    console.log(layouts);
-  }
 }
 
 const LayoutHistory = new _LayoutHistory();
+
+LayoutHistory.dispatchToken = AppDispatcher.register(function(payload) {
+  var action = payload.action;
+
+  switch (action.type) {
+    case LayoutActionTypes.GENERATE_LAYOUT:
+      _saveLayout();
+      LayoutHistory.emitChange();
+      break;
+    case LayoutActionTypes.SET_WIDTH:
+      _saveLayout();
+      LayoutHistory.emitChange();
+      break;
+    case LayoutActionTypes.SET_HEIGHT:
+      _saveLayout();
+      LayoutHistory.emitChange();
+      break;
+    default:
+  }
+});
 
 export default LayoutHistory;
