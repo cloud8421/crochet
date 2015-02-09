6e02d7143431253a8cca91aea80fef8cbfd9ee3e
Claudio Ortolina <claudio@new-bamboo.co.uk>
2015-02-09 17:41:47 +0000

    Show more data in history.

--- src/scripts/components/history/layout.js
+++ src/scripts/components/history/layout.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import ImmutableRenderMixin from 'react-immutable-render-mixin';
 import LayoutActions from '../../actions/LayoutActions';
+import ColorPreview from '../shared/ColorPreview';
 
 const Layout = React.createClass({
   mixins: [ImmutableRenderMixin],
   render() {
     let colorsCount = this.props.layout.colors.size;
 
+    let colors = this.props.layout.colors.map(function(color) {
+      return (
+        <li>
+          <ColorPreview color={color} />
+        </li>
+      )
+    });
+
     return (
       <li>
-        <span>{colorsCount} colors</span>
+        <strong>{this.props.layout.width}x{this.props.layout.height}</strong>
+        <ul className="colors">
+          {colors.toArray()}
+        </ul>
         <button onClick={this.restoreLayout}>Restore</button>
       </li>
     )
