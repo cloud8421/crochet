975255fd5e23245b96105dbe7195bac7fd14e234
Claudio Ortolina <claudio@new-bamboo.co.uk>
2015-02-19 12:39:06 +0000

    Adds Firebase auth (shows profile for now).

--- src/scripts/components/Navigation.js
+++ src/scripts/components/Navigation.js
@@ -1,18 +1,48 @@
 import React from 'react';
+import Fbase from '../stores/Fbase';
+
+let userBar = function(name, avatar) {
+  return (
+    <div className="user">
+      <img src={avatar} />
+      <span className="name">{name}</span>
+    </div>
+  )
+}
 
 const Navigation = React.createClass({
+  getInitialState (){
+    return {
+      user: {
+        name: null,
+        avatar: null
+      }
+    }
+  },
   render (){
+    let loginLink = <div className="login">
+      <a onClick={this.loginWithFacebook}>Login with Facebook</a>
+    </div>
+    let user = this.state.user;
     return (
       <header role="banner" className="navigation">
         <h1>Crochet</h1>
-        <div className="login">
-          <a onClick={this.loginWithFacebook}>Login with Facebook</a>
-        </div>
+        {user.name ? userBar(user.name, user.avatar) : loginLink}
       </header>
     )
   },
   loginWithFacebook() {
-    console.log('should login with Facebook');
+    let success = function(data) {
+      let profile = data.facebook.cachedUserProfile;
+      this.setState({
+        user: {
+          name: profile.name,
+          avatar: profile.picture.data.url
+        }
+      });
+    }.bind(this);
+    let damn = function(error) { console.error(error) };
+    Fbase.auth(success, damn);
   }
 });
 
---
+++ src/scripts/stores/Fbase.js
@@ -0,0 +1,20 @@
+import Firebase from 'firebase';
+
+const FIREBASE_URL = "https://torid-torch-7672.firebaseio.com";
+
+class Fbase {
+  constructor() {
+    this.ref = new Firebase(FIREBASE_URL);
+  }
+  auth(successCb, errorCb) {
+    this.ref.authWithOAuthPopup("facebook", function(error, authData) {
+      if (error) {
+        errorCb(error);
+      } else {
+        successCb(authData);
+      }
+    });
+  }
+}
+
+export default new Fbase();
